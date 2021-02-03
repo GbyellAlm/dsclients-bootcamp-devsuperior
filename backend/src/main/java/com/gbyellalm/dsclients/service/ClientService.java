e4f2a40f72d22db370aa9a11450b71d64e65e477
GbyellAlm <gabriel_almeida@edu.univali.br>
2021-02-03 19:51:15 -0300

    Get client by id, and ResourceNotFoundException

--- backend/src/main/java/com/gbyellalm/dsclients/service/ClientService.java
+++ backend/src/main/java/com/gbyellalm/dsclients/service/ClientService.java
@@ -1,5 +1,7 @@
 package com.gbyellalm.dsclients.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.gbyellalm.dsclients.dto.ClientDTO;
 import com.gbyellalm.dsclients.entitiy.Client;
 import com.gbyellalm.dsclients.repository.ClientRepository;
+import com.gbyellalm.dsclients.service.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -20,5 +23,11 @@ public class ClientService {
 	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
 		Page<Client> list = repository.findAll(pageRequest);
 		return list.map(listElement -> new ClientDTO(listElement)); 
+	}
+
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Cliente não encontrado"));
+		return new ClientDTO(entity);
 	}
 }
