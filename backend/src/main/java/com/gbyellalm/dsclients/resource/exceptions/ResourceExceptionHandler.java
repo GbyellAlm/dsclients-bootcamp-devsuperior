bc0bcbf8f0de540b96fafd36a27354de38a3a246
GbyellAlm <gabriel_almeida@edu.univali.br>
2021-02-03 21:13:14 -0300

    Post client, StandardError and ResourceExceptionHandler

---
+++ backend/src/main/java/com/gbyellalm/dsclients/resource/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.gbyellalm.dsclients.resource.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.gbyellalm.dsclients.service.exceptions.ResourceNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Cliente não encontrado");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
